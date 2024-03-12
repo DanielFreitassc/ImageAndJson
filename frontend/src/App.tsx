32f3898b11b81437d4c06427038fda139af92a82
danielfreitassc <danielfreitassc@gmail.com>
2024-03-12 09:52:59 -0300

    Front funcionando

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,15 @@ import { FoodData } from './components/interface/FoodData';
 import { Card } from './components/card';
 import { useFoodData } from './components/hooks/useFoodData';
 import './App.css'
+import { CreateModal } from './components/create-model/create-modal';
 
 function App() {
   const {data} = useFoodData();
+  const [isModalOpen, setIsModalOpen]= useState(false);
 
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
     <div className="container">
@@ -20,6 +25,8 @@ function App() {
           />
         )}
       </div>
+        {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+        <button onClick={handleOpenModal}>novo</button>
       
     </div>
   )
